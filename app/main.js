cc0c28f07d524094d29603f792121a3a555f2871
颜廷宇（本地） <yanty@neusoft.com>
2014-02-19 21:50:24 +0800

    简单main.js框架

---
+++ app/main.js
@@ -0,0 +1,25 @@
+/**
+ * 配置requirejs
+ */
+requirejs.config({
+    baseUrl: 'modules',
+    shim: {},
+    paths: {
+        // JS库，替代jquery（http://www.zeptojs.com/）
+        'zepto': './libs/zepto',
+        // requirejs文本插件（http://www.requirejs.org/）
+        'text': './libs/text',
+        // requirejs模板插件
+        'tpl': './libs/tpl',
+        // 手势触摸组件（http://eightmedia.github.io/hammer.js/）
+        'hammer': './libs/hammer',
+        // iScroll5页面滚动组件（http://cubiq.org/iscroll-5）
+        'iscroll': './libs/iscroll',
+        // Bootstrap3样式组件
+        'bootstrap': './libs/bootstrap'
+    }
+});
+
+requirejs(['mainUI'], function (App) {
+    return new App();
+});
